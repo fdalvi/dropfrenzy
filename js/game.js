07737d412310a01f8df0854c3784cf881e773504
Fahim Imaduddin Dalvi <fid@qatar.cmu.edu>
2013-06-07 15:36:43 +0300

    Replaced setTimeout with requestAnimFrame for better animation performance

--- js/game.js
+++ js/game.js
@@ -12,28 +12,35 @@ var keys = [];
 
 // HTML5 onLoad event
 Event.observe(window, 'load', function() {
-	world = createWorld(); // box2DWorld 
-	ctx = $('game').getContext('2d'); // 2
+	// Creating Box2DWorld
+	world = createWorld();
+
+	// Grabbing reference to 2D Canvas
+	ctx = $('game').getContext('2d'); 
 	var canvasElm = $('game');
 	canvasWidth = parseInt(canvasElm.width);
 	canvasHeight = parseInt(canvasElm.height);
-	initGame(); // 3
-	step(); // 4
-	
-// 5
+
+	// Initialize Game
+	initGame(); 
+
+	// Event Listeners for keys to avoid page scrolling
 	window.addEventListener('keydown',handleKeyDown,true); 
 	window.addEventListener('keyup',handleKeyUp,true);
+
+	// Run game loop
+	animate();
 });
 
 function initGame(){
-	// create 2 big platforms	
-	createBox(world, 3, 230, 60, 180, true, 'ground');
-	createBox(world, 560, 360, 50, 50, true, 'ground');
-	
-	// create small platforms
-	for (var i = 0; i < 5; i++){
-		createBox(world, 150+(80*i), 360, 15, 40, true, 'ground');	
-	}
+	// Create Map	
+	createBox(world, 50, 50, 100, 10, true, 'ground');
+	createBox(world, 550, 50, 100, 10, true, 'ground');
+	createBox(world, 300, 100, 150, 10, true, 'ground');
+	createBox(world, 300, 150, 150, 10, true, 'ground');
+	createBox(world, 300, 200, 150, 10, true, 'ground');
+	createBox(world, 300, 250, 150, 10, true, 'ground');
+
 	
 	// create player ball
 	var ballSd = new b2CircleDef();
@@ -51,9 +58,41 @@ function initGame(){
 	
 }
 
-function step() {
-	
+window.requestAnimFrame = (function(callback) {
+	return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+	function(callback) {
+		window.setTimeout(callback, 1000 / 60);
+	};
+})();
 
+var setupFps = function(){
+
+	var lastTime = new Date();
+	var hits = 0;
+	var fps = "Waiting";
+	var hit = function(){
+		hits++;
+		var nowTime = new Date();
+		if (nowTime.getTime() - lastTime.getTime() > 1000){
+			var dt = nowTime.getTime() - lastTime.getTime();
+			fps = "" + Math.round(hits * 1000 / dt);
+			hits = 0;
+			lastTime = nowTime;
+		}
+		return fps;
+	};
+	return hit;
+};
+
+
+var hit = setupFps();
+
+
+
+function animate() {
+	requestAnimFrame(function() {
+		animate();
+	});
 	if (player.object.GetCenterPosition().y > canvasHeight){
 		player.object.SetCenterPosition(new b2Vec2(20,0),0)
 	}	
@@ -64,17 +103,17 @@ function step() {
 
 	handleInteractions();
 
-
 	var stepping = false;
-	var timeStep = 1.0/60;
+	var timeStep = 1.0/30;
 	var iteration = 1;
-	// 1
 	world.Step(timeStep, iteration);
-	// 2
 	ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 	drawWorld(world, ctx);
-	// 3
-	setTimeout('step()', 10);
+
+	ctx.fillStyle    = '#000';
+	ctx.font         = '30px verdana';
+	ctx.textBaseline = 'top';
+	ctx.fillText(hit(), 30, 100);
 }
 
 function showWin(){
